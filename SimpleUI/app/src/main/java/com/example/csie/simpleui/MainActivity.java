f5748795ca800818b847ec032f3bf05075a2fa8d
godgunman <godgunman@gmail.com>
2015-03-19 19:32:18 +0800

    add Parse lib

--- SimpleUI/app/src/main/java/com/example/csie/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/csie/simpleui/MainActivity.java
@@ -22,6 +22,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -52,6 +55,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this,
+                "ucTD3rqecD1jHY8NGDHDUhrypkYeiVYxfb7bYjGH",
+                "0FQy4fMeCHavguzxpuLGUqFXW4G2j1D6ruilPInS");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
 
         sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
         editor = sp.edit();
